39495dcd0570b8d176b869fb80ae3dd5f80b7fbb
joey0320 <joonho0320@gmail.com>
2020-08-14 02:27:23 +0900

    update lca

--- graph/lca.cpp
+++ graph/lca.cpp
@@ -1,68 +1,31 @@
-// LCA tree;
-// tree.init(N);
-// tree.assignLevel(root);
+// dfs(root, root)
+// lca(x, y)
+const int maxn = 2e5+5;
 
-struct LCA {
-	int n, k;
-	vector<vector<int>> graph, par;
-	vector<int> dist;
+int tin[maxn], tout[maxn], par[maxn], up[maxn][logn];
+vector<int> graph[maxn];
+int root, T;
 
-	void init ( int sz ) {
-		n = sz;
-		k = (int) ceil(log2(n)) + 1;
-		graph.resize(n);
-		par.resize(n);
-		for ( int i = 0; i < n; i ++ ) par[i].assign(k, -1);
-		dist.assign(n, -1);
-	}
+void dfs ( int u, int p ) {
+    tin[u] = ++T;
+    par[u] = p;
+    up[u][0] = p;
+    for ( int i = 0; i < logn - 1; i++ ) {
+        int mid = up[u][i];
+        up[u][i+1] = up[mid][i];
+    }
+    for ( auto v : graph[u] ) dfs(v, u);
+    tout[u] = ++T;
+}
 
-	void addedge ( int u, int v ) {
-		graph[u].push_back(v);
-		graph[v].push_back(u);
-	}
+bool is_ancestor ( int x, int y ) {
+    return tin[x] <= tin[y] && tout[y] <= tout[x];
+}
 
-	void dfs ( int u, int p ) {
-		if ( p == - 1 ) dist[u] = 0;
-		for ( auto v : graph[u] ) if ( v != p ) {
-			dist[v] = dist[u] + 1;
-			par[v][0] = u;
-			dfs(v, u);
-		}
-	}
-
-	void assignParent ( int root ) {
-		dfs(root, -1);
-
-		for ( int j = 0; j < k; j++ ) {
-			for ( int i = 0; i < n; i++ ) {
-				int mid = par[i][j];
-				if ( mid != -1 ) par[i][j + 1] = par[mid][j];
-			}
-		}
-	}
-
-	int solve ( int x, int y ) {
-		if ( dist[x] < dist[y] ) swap(x, y);
-
-		int diff = dist[x] - dist[y];
-		int a = 0;
-		while ( diff ) {
-			if ( diff & 1 ) x = par[x][a];
-			a++;
-			diff >>= 1;
-		}
-
-		if ( x != y ) {
-			for ( int i = k - 1; i >=0; i-- ) {
-				if ( par[x][i] != -1 && par[x][i] != par[y][i] ) {
-					x = par[x][i];
-					y = par[y][i];
-				}
-			}
-			x = par[x][0];
-			y = par[y][0];
-		}
-		return x;
-	}
-};
-
+int get_lca ( int x, int y ) {
+    if ( is_ancestor(x, y) ) return x;
+    for ( int i = logn - 1; i >= 0; i-- ) 
+        if ( !is_ancestor(up[x][i], y) ) 
+            x = up[x][i];
+    return up[x][0];
+}
